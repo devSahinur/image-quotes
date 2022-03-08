407cec93397ed82a1a5bf5b4a9e940ceb02f770c
Sahinur Islam <infosahinur@gmail.com>
2022-03-09 00:33:56 +0600

    Header added

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+
+
+function Header() {
+  return (
+    <div className="flex justify-center gap-1 absolute w-full mt-10 text-3xl font-black">
+      <h1 className="text-indigo-500">IMAGE</h1>{" "}
+      <h1 className="text-indigo-900">QUOTES</h1>
+    </div>
+  )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-    <h1 className='text-red-700'>hello</h1>
+      <Head>
+        <title>Image Quotes</title>
+      </Head>
+      <h1 className="text-red-700">hello</h1>
     </>
-  )
+  );
 }
