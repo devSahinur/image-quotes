a0a89652ac78c8970b732b41bc2987803d321d32
Sahinur Islam <infosahinur@gmail.com>
2022-03-09 00:28:51 +0600

    project set up done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      md: [{ max: "750px" }],
+    },
+
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
